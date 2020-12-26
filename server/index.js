86557e26e1f736fbec984425e2cb92288b71a5fd
Matt Brown <matthewbrown0798@gmail.com>
2020-12-26 16:01:27 -0500

    connected server to mongoAtlas db

--- server/index.js
+++ server/index.js
@@ -2,3 +2,27 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+
+app.use(cors());
+
+//MongoDB Atlas connection string
+const CONNECTION_URL =
+  "mongodb+srv://mbrown98:Ras63206@cluster0.zdhlk.mongodb.net/<dbname>?retryWrites=true&w=majority";
+
+const PORT = process.env.port || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => app.listen(PORT, () => console.log("Connected on port " + PORT)))
+  .catch((err) => console.log(err.message));
+
+mongoose.set("useFindAndModify", false);

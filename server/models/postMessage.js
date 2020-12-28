dc461500d657b000ca2c17b12fd2098a04e02a3a
Matt Brown <matthewbrown0798@gmail.com>
2020-12-28 14:12:44 -0500

    completed getPosts and createPost controllers

--- server/models/postMessage.js
+++ server/models/postMessage.js
@@ -4,7 +4,7 @@ const postSchema = mongoose.Schema({
   title: String,
   message: String,
   creator: String,
-  tags: [Strings],
+  tags: [String],
   selectedFile: String,
   likeCount: {
     type: Number,

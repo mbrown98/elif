5e0d010a0f3579cec6d1761e58a2b267f7e7e085
Matt Brown <matthewbrown0798@gmail.com>
2020-12-29 13:57:46 -0500

    like post functionality

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -49,3 +49,21 @@ export const deletePost = async (req, res) => {
   await PostMessage.findByIdAndRemove(_id);
   res.json({ message: "Post deleted successfully" });
 };
+
+export const likePost = async (req, res) => {
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).send("no post with that id");
+  }
+  const post = await PostMessage.findById(id);
+
+  const updatedPost = await PostMessage.findByIdAndUpdate(
+    id,
+    {
+      likeCount: post.likeCount + 1,
+    },
+    { new: true }
+  );
+
+  res.json(updatedPost);
+};

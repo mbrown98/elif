10e0c4700eb747eeb50c7a05e640d0d6a0159c82
Matt Brown <matthewbrown0798@gmail.com>
2020-12-29 13:45:26 -0500

    delete post functionality

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -38,3 +38,14 @@ export const updatePost = async (req, res) => {
 
   res.json(updatedPost);
 };
+
+export const deletePost = async (req, res) => {
+  const { id: _id } = req.params;
+  const post = req.body;
+  if (!mongoose.Types.ObjectId.isValid(_id)) {
+    return res.status(404).send("no post with that id");
+  }
+
+  await PostMessage.findByIdAndRemove(_id);
+  res.json({ message: "Post deleted successfully" });
+};
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,5 +1,10 @@
 import express from "express";
-import { getPosts, createPost, updatePost } from "../controllers/posts.js";
+import {
+  getPosts,
+  createPost,
+  updatePost,
+  deletePost,
+} from "../controllers/posts.js";
 
 const router = express.Router();
 
@@ -7,5 +12,7 @@ router.get("/", getPosts);
 router.post("/", createPost);
 
 router.patch("/:id", updatePost);
+
+router.delete("/:id", deletePost);
 
 export default router;
